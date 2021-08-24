f132940198cecbdcb6866fac2598c32704d3751b
Smerlyn Alejandro <smerlyn_alejandro@outlook.com>
2021-08-23 21:32:31 -0400

    Agregando partial para error

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,18 @@ exports.GetRegistro = function(req, res, next) {
 };
 
 exports.PostRegistro = function(req, res, next){
-    console.log(req.body)
-    res.send("Nice")
+    const { nombre, apellido, email, usuario, contrasena, c_contrasena} = req.body
+    const error = [];
+    if(contrasena != c_contrasena){
+        error.push({text: "Las contraseñas no coinciden"})
+    }
+    if(contrasena.length > 4){
+        error.push({text: "La contraseña debe tener más de 4 dígitos"})
+    }
+    if(error.length > 0){
+        res.render('registro', {error, nombre, apellido, email, usuario, contrasena, c_contrasena});
+    } else{
+        res.send("Nice")
+    }
+    
 }
