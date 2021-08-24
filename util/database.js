4acd380869fa93e6124a1905e27f31bb2e6ae274
Smerlyn Alejandro <smerlyn_alejandro@outlook.com>
2021-08-23 20:01:51 -0400

    View/auth/registro.hbs: modifiqué esta vista para que quede mejor en la página

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize('sistema_de_elecciones', 'root', '', {
     host: 'localhost',
     dialect: "mysql",
     //Aqui lo tuve que cambiar a mi puerto
-    port: 3301
+    port: 3306
 
 });
 
